71dfea8d16af77500f62ef3c7f11d1c9e1554f05
Chet Gray <chetgray@gmail.com>
2018-05-12 15:10:37 -0400

    Begin shopping list script
    https://teamtreehouse.com/library/shopping-list-introduction

---
+++ shopping_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Shopping List To-Do List
+
+*   Run the script to start using it
+*   Put new things in the list, one at a time
+*   Enter the word DONE - in all caps - to quit the program
+*   And, once I quit, I want the app to show me everything that's
+    on my list
+
+https://teamtreehouse.com/library/shopping-list-introduction
+"""
