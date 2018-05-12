afdcb128308523094200fd9d3c580e7494526f84
Chet Gray <chetgray@gmail.com>
2018-05-12 16:52:54 -0400

    Begin number_game.py
    https://teamtreehouse.com/library/number-game-introduction

---
+++ number_game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+
+*   generate a random number between 1 and 10
+*   get a number guess from the player
+*   compare guess to secret number
+*   print hit/miss
+
+https://teamtreehouse.com/library/number-game-introduction
+"""
