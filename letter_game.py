4091654eaf3ae78261760f806be24ece1a646b0a
Chet Gray <chetgray@gmail.com>
2018-05-12 18:30:13 -0400

    Begin letter game
    https://teamtreehouse.com/library/letter-game-introduction

---
+++ letter_game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+Pick a random word from a list and then let the user guess letters
+until they either get all the letters in the word or they run out of
+chances.
+
+*   make a list of words
+*   pick a random word
+*   draw spaces
+*   take guesses
+*   draw guessed letters and strikes
+*   print out win/lose
+"""
