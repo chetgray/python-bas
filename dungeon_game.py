ac05bfd1e0d06ad57e3d5cf7ea21390fc6132d83
Chet Gray <chetgray@gmail.com>
2018-07-22 13:09:06 -0400

    Begin dungeon game

---
+++ dungeon_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+# draw grid
+# pick random location for player
+# pick random location for exit door
+# pick random location for the monster
+# draw player in the grid
+# take input for movement
+# move player, unless invalid move (past edges of grid)
+# check for win/loss
+# clear screen and redraw grid
